171efca0aa79246fb4357f9a507c8e14d2afa0c8
dirkarnez <smalldirkalex@gmail.com>
2021-12-08 18:08:36 +0800

    - add linked list

--- src/main.c
+++ src/main.c
@@ -1,54 +1,168 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <string.h>
+#include <stdbool.h>
 
-typedef struct {
-    char name[50];
-    char phone_number[50];
-    char email[50];
+#define FILE_NAME "data.dat"
+#define STRING_MAX_LEN 50
+
+typedef struct user
+{
+    char name[STRING_MAX_LEN];
+    char phone_number[STRING_MAX_LEN];
+    char email[STRING_MAX_LEN];
+    struct user *next;
 } User;
 
-int main() {
-    User* user_ptr_list[100];
+bool match(char* to_search, char* target) {
+    return strstr(to_search, target) != NULL;
+}
+
+char* random_email(unsigned int length_greater_than_11) {
+    const char* suffix = "@gmail.com";
+    // 11 reserved, with \0
+    
+    const unsigned int random_part_length = length_greater_than_11 - strlen(suffix) - 1;
+    
+    char *random_part = malloc((random_part_length * sizeof(char)) + 1);
+    char *output = malloc(length_greater_than_11 * sizeof(char));
+    
+    srand((unsigned int)(time(NULL)));
+
+    const char random_char_list[] = "abcdefg";
+
+    unsigned int i = 0;
+    for(i = 0; i < random_part_length; i++)
+	{
+	    random_part[i] = random_char_list[rand() % (sizeof random_char_list - 1)];
+	}
+    random_part[random_part_length] = '\0';
+
+    snprintf(output, length_greater_than_11, "%s%s", random_part, suffix);
+	return output;
+}
+
+void print_linked_list(User* first_user_ptr) {
+    User* ptr = first_user_ptr;
+    while (ptr != NULL)
+    {
+        printf("address = %p, ", ptr);
+        printf("name = \"%s\" ", ptr->name);
+        printf("phone number = \"%s\" ", ptr->phone_number);
+        printf("email = \"%s\" ", ptr->email);
+        printf("next = %p\n", ptr->next);
+        ptr = ptr->next;
+    }
+}
+
+void save_linked_list_to_file(FILE* fp, User* first_user_ptr) {
+    User* ptr = first_user_ptr;
+    while (ptr != NULL)
+    {
+        fprintf(fp, "%s %s %s\n", ptr->name, ptr->phone_number, ptr->email);
+        ptr = ptr->next;
+    }
+}
+
+int main()
+{
     FILE *fp = NULL;
 
-    fp = fopen("data.dat", "r");
-    if (fp == NULL) {
-        return 1;
-    }
-
-    User* user = NULL;
-    int i = 0;
-    while ((user = (User*)malloc(sizeof(User))) && (fscanf(fp,"%s %s %s\n", user->name, user->phone_number, user->email) != EOF))
+    while ((fp = fopen(FILE_NAME, "r+")) == NULL)
     {
-        printf("in while %s %s %s\n", user->name, user->phone_number, user->email); 
-        user_ptr_list[i] = user;
-        i++;
+        printf("%s not exists, creating...\n", FILE_NAME);
+        fp = fopen(FILE_NAME, "w+");
+        if (fp != NULL)
+        {
+            printf("%s is created!\n", FILE_NAME);
+            fclose(fp);
+        }
+    }
+    printf("reading %s!\n", FILE_NAME);
+
+    User *first_user_ptr = NULL;
+    User *prev_user_ptr = NULL;
+    User *current_user_ptr = NULL;
+
+    while (
+        (current_user_ptr = (User *)malloc(sizeof(User))) &&
+        (fscanf(fp, "%s %s %s\n",
+                current_user_ptr->name,
+                current_user_ptr->phone_number,
+                current_user_ptr->email) != EOF))
+    {
+        if (first_user_ptr == NULL)
+        {
+            first_user_ptr = current_user_ptr;
+        }
+
+        
+        if (prev_user_ptr != NULL)
+        {
+            prev_user_ptr->next = current_user_ptr;
+        }
+
+        current_user_ptr->next = NULL;
+
+        prev_user_ptr = current_user_ptr;
     }
 
-    fclose(fp);
+    printf("file content is stored in linkedlist!\n");
 
-    fp = fopen("data.dat", "w");
+    printf("\n");
+    {
+        printf("Now we print out our linkedlist as below!\n");
+        print_linked_list(first_user_ptr);
 
-    printf("random after w: %s %s %s\n", user_ptr_list[0]->name, user_ptr_list[0]->phone_number, user_ptr_list[0]->email); 
-
-    User *newUser= (User*)malloc(sizeof(User));   
-    strcpy(newUser->name, "7");  
-    strcpy(newUser->phone_number, "8");  
-    strcpy(newUser->email, "9");
-    printf("insert newUser: %s %s %s\n", newUser->name, newUser->phone_number, newUser->email); 
-
-    user_ptr_list[i] = newUser;
-    i += 1;
-
-    int j = 0;
-    for (int j = 0; j < i; j++) {
-        fprintf(fp, "%s %s %s\n", user_ptr_list[j]->name, user_ptr_list[j]->phone_number, user_ptr_list[j]->email);
     }
 
-    fclose(fp);
+    printf("\n");
+
+    {
+        printf("Now we modify our linkedlist!\n");
+        printf("Say, we search for the first user name \"peter\" (case sensitive), then we modify his email!\n");
+
+        User* ptr = first_user_ptr;
+
+        while (ptr != NULL)
+        {
+            printf("address = %p, ", ptr);
+            printf("name = \"%s\" ", ptr->name);
+            printf("phone number = \"%s\" ", ptr->phone_number);
+            printf("email = \"%s\" ", ptr->email);
+
+            if (match(ptr->name, "peter")) {
+                printf("(We find \"%s\" here!), generating new email... ", ptr->name);
+                strcpy(ptr->email, random_email(20));
+                printf("updated email = %s ", ptr->email);
+            }
+
+            printf("next = %p\n", ptr->next);
+            ptr = ptr->next;
+        }
+    }
+
+    printf("\n");
+    {
+        printf("Now we check our linkedlist!\n");
+        print_linked_list(first_user_ptr);
+    }
+
+
+    printf("\n");
+    {
+        printf("Now we save our linkedlist to %s!\n", FILE_NAME);
+        fclose(fp);
+
+        fp = fopen("data.dat", "w");
+        save_linked_list_to_file(fp, first_user_ptr);
+        fclose(fp);
+    }
+
+    printf("\n");
+    printf("DONE! Bye bye!\n");
 
     system("pause");
     return 0;
 }
- 
